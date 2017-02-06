7a0346e1bca61bb62857b11ac27d43d71797a91b
Jorge Sanchez <jorge.sanchez@wipro.com>
2017-02-06 12:13:33 -0500

    updating header comp

--- components/2-Molecules/TacoCrumbs.jsx
+++ components/2-Molecules/TacoCrumbs.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { browserHistory } from 'react-router';
 
 const TacoCrumbs = ({ viewName }) => (
-  <div className="taco-crumbs">
-    <span className="back-icon" onClick={browserHistory.goBack}><img src="../assets/images/back-button.png" /></span>
-    <span className="action">{ viewName }</span>
-    <span className="cart-icon"><Link to="/cart"><img src="../assets/images/cart-icon.png" /></Link></span>
-  </div>
+  <div className="header">
+    <div className="back-button">back</div>
+    <div className="header-text">{ viewName }</div>
+    <div className="cart-button">cart</div>
+
+</div>
 )
 
 export default TacoCrumbs;
