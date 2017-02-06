bdade2026f48666a4517bf9695afd8ba3499fe41
Zachary Smith <william.smith1@wipro.com>
2017-02-06 15:12:52 -0500

    Pass class names down as "styles" prop for MenuItem
    This allows us to define classes names in the consuming app, which avoids relying on convention to match CSS classes to components.

--- components/2-Molecules/MenuItem.jsx
+++ components/2-Molecules/MenuItem.jsx
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import PlaceholderImage from '../1-Atoms/PlaceholderImage';
 import QuantityPicker from '../1-Atoms/QuantityPicker';
 
-const MenuItem = ({ name, price, quantity = 0 }) => (
-    <div className="menu-item">
+const MenuItem = ({ name, price, quantity = 0, styles = {} }) => (
+    <div className={styles['menu-item'] || 'menu-item'}>
       <PlaceholderImage width={160} height={160} />
-      <div className="info">
-          <span className="name">{name}</span>
-          <span className="price">{price}</span>
+      <div className={styles['info'] || 'info'}>
+          <span className={styles['name'] || 'name'}>{name}</span>
+          <span className={styles['price'] || 'price'}>{price}</span>
       </div>
       <QuantityPicker quantity={6} />
     </div>
